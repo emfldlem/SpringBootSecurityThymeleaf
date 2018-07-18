f9ebc219beaa5b35b3ac7245b00d14dd7e31a85a
swkim <swkim@bsgglobal.com>
2018-07-18 17:13:16 +0900

    global properties 설정

--- src/main/java/com/template/sbssth/Controller/DefaultController.java
+++ src/main/java/com/template/sbssth/Controller/DefaultController.java
@@ -1,13 +1,32 @@
 package com.template.sbssth.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PropertySource("classpath:global.properties")
 public class DefaultController {
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${email}")
+    private String email;
+
+    @Value("${Sources}")
+    private String Sources;
+
     @GetMapping("/")
-    public String home1() {
+    public String home1(Model model) {
+
+        model.addAttribute("msg", "<h1>END</h1><br /><h2>END</h2>");
+        model.addAttribute("msg", "<h1>END</h1><br /><h2>END</h2>");
+        model.addAttribute("msg", "<h1>END</h1><br /><h2>END</h2>");
+
+
         return "/home";
     }
 
@@ -32,7 +51,12 @@ public class DefaultController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("Sources", Sources);
+
+
         return "/login";
     }
 
